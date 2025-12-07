606dca52f2343d8c320ce37709ede46ff3030c64
W-Bjwa04 <waleedshahid123ml@gmail.com>
2025-12-07 13:44:00 +0500

    Add comprehensive Chrome flags for SSL and mixed content handling

--- tests/test-config.js
+++ tests/test-config.js
@@ -12,15 +12,31 @@ function getChromeOptions() {
         options.addArguments('--headless=new');
     }
 
+    // Core security and sandbox flags
     options.addArguments('--no-sandbox');
     options.addArguments('--disable-dev-shm-usage');
     options.addArguments('--disable-gpu');
     options.addArguments('--window-size=1920,1080');
     options.addArguments('--disable-extensions');
     options.addArguments('--disable-software-rasterizer');
+    
+    // SSL/Certificate handling
     options.addArguments('--ignore-certificate-errors');
+    options.addArguments('--ignore-ssl-errors');
+    options.addArguments('--ignore-certificate-errors-spki-list');
     options.addArguments('--allow-insecure-localhost');
     options.addArguments('--disable-web-security');
+    options.addArguments('--allow-running-insecure-content');
+    
+    // Disable features that might interfere
+    options.addArguments('--disable-features=VizDisplayCompositor');
+    options.addArguments('--disable-blink-features=AutomationControlled');
+    
+    // Set preferences to avoid SSL issues
+    options.setUserPreferences({
+        'profile.default_content_setting_values.mixed_content': 1,
+        'profile.managed_default_content_settings.mixed_content': 1
+    });
 
     return options;
 }
